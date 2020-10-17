9fce341455318cb52b806243a0d40873b59f01db
Charles Lew <crlf0710@gmail.com>
2020-10-17 12:12:17 +0800

    Update license info.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,53 @@
-// This program is copyright (C) 1982 by D. E. Knuth; all rights are reserved.
+// Copyright 2020 TeX-rs Author(s)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------OR----------------------------
+// Copyright 2020 TeX-rs Author(s)
+//
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// -------------------------------------------------------
+// `TeX-rs` is a port of the `TeX` program to Rust.
+// The original `TeX` program is copyright (C) 1982
+// by D. E. Knuth; all rights are reserved.
 
 // TeX is a trademark of the American Mathematical Society.
 // METAFONT is a trademark of Addison-Wesley Publishing Company.
 
+// Notes from the original author:
+//
 // Although considerable effort has been expended to make the TeX program
 // correct and reliable, no warranty is implied; the author disclaims any
 // obligation or liability for damages, including but not limited to
