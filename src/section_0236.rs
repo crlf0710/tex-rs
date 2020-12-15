fc3ad2c73b40529ff9dc2a79442d3f507032891b
Charles Lew <crlf0710@gmail.com>
2020-12-16 01:12:18 +0800

    Add all the int registers.

--- src/section_0236.rs
+++ src/section_0236.rs
@@ -7,28 +7,64 @@
 //! that will be defined later.
 //
 // @d pretolerance_code=0 {badness tolerance before hyphenation}
+/// badness tolerance before hyphenation
+pub(crate) const pretolerance_code: halfword = 0;
 // @d tolerance_code=1 {badness tolerance after hyphenation}
+/// badness tolerance after hyphenation
+pub(crate) const tolerance_code: halfword = 1;
 // @d line_penalty_code=2 {added to the badness of every line}
+/// added to the badness of every line
+pub(crate) const line_penalty_code: halfword = 2;
 // @d hyphen_penalty_code=3 {penalty for break after discretionary hyphen}
+/// penalty for break after discretionary hyphen
+pub(crate) const hyphen_penalty_code: halfword = 3;
 // @d ex_hyphen_penalty_code=4 {penalty for break after explicit hyphen}
+/// penalty for break after explicit hyphen
+pub(crate) const ex_hyphen_penalty_code: halfword = 4;
 // @d club_penalty_code=5 {penalty for creating a club line}
+/// penalty for creating a club line
+pub(crate) const club_penalty_code: halfword = 5;
 // @d widow_penalty_code=6 {penalty for creating a widow line}
 /// penalty for creating a widow line
 pub(crate) const widow_penalty_code: halfword = 6;
 // @d display_widow_penalty_code=7 {ditto, just before a display}
+/// ditto, just before a display
+pub(crate) const display_widow_penalty_code: halfword = 7;
 // @d broken_penalty_code=8 {penalty for breaking a page at a broken line}
+/// penalty for breaking a page at a broken line
+pub(crate) const broken_penalty_code: halfword = 8;
 // @d bin_op_penalty_code=9 {penalty for breaking after a binary operation}
+/// penalty for breaking after a binary operation
+pub(crate) const bin_op_penalty_code: halfword = 9;
 // @d rel_penalty_code=10 {penalty for breaking after a relation}
+/// penalty for breaking after a relation
+pub(crate) const rel_penalty_code: halfword = 10;
 // @d pre_display_penalty_code=11
 //   {penalty for breaking just before a displayed formula}
+/// penalty for breaking just before a displayed formula
+pub(crate) const pre_display_penalty_code: halfword = 11;
 // @d post_display_penalty_code=12
 //   {penalty for breaking just after a displayed formula}
+/// penalty for breaking just after a displayed formula
+pub(crate) const post_display_penalty_code: halfword = 12;
 // @d inter_line_penalty_code=13 {additional penalty between lines}
+/// additional penalty between lines
+pub(crate) const inter_line_penalty_code: halfword = 13;
 // @d double_hyphen_demerits_code=14 {demerits for double hyphen break}
+/// demerits for double hyphen break
+pub(crate) const double_hyphen_demerits_code: halfword = 14;
 // @d final_hyphen_demerits_code=15 {demerits for final hyphen break}
+/// demerits for final hyphen break
+pub(crate) const final_hyphen_demerits_code: halfword = 15;
 // @d adj_demerits_code=16 {demerits for adjacent incompatible lines}
+/// demerits for adjacent incompatible lines
+pub(crate) const adj_demerits_code: halfword = 16;
 // @d mag_code=17 {magnification ratio}
+/// magnification ratio
+pub(crate) const mag_code: halfword = 17;
 // @d delimiter_factor_code=18 {ratio for variable-size delimiters}
+/// ratio for variable-size delimiters
+pub(crate) const delimiter_factor_code: halfword = 18;
 // @d looseness_code=19 {change in number of lines for a paragraph}
 /// change in number of lines for a paragraph
 pub(crate) const looseness_code: halfword = 19;
@@ -45,10 +81,20 @@ pub(crate) const month_code: halfword = 22;
 /// current year of our Lord
 pub(crate) const year_code: halfword = 23;
 // @d show_box_breadth_code=24 {nodes per level in |show_box|}
+/// nodes per level in `show_box`
+pub(crate) const show_box_breadth_code: halfword = 24;
 // @d show_box_depth_code=25 {maximum level in |show_box|}
+/// maximum level in `show_box`
+pub(crate) const show_box_depth_code: halfword = 25;
 // @d hbadness_code=26 {hboxes exceeding this badness will be shown by |hpack|}
+/// hboxes exceeding this badness will be shown by `hpack`
+pub(crate) const hbadness_code: halfword = 26;
 // @d vbadness_code=27 {vboxes exceeding this badness will be shown by |vpack|}
+/// vboxes exceeding this badness will be shown by `vpack`
+pub(crate) const vbadness_code: halfword = 27;
 // @d pausing_code=28 {pause after each line is read from a file}
+/// pause after each line is read from a file
+pub(crate) const pausing_code: halfword = 28;
 // @d tracing_online_code=29 {show diagnostic output on terminal}
 /// show diagnostic output on terminal
 pub(crate) const tracing_online_code: halfword = 29;
@@ -56,9 +102,17 @@ pub(crate) const tracing_online_code: halfword = 29;
 /// show macros as they are being expanded
 pub(crate) const tracing_macros_code: halfword = 30;
 // @d tracing_stats_code=31 {show memory usage if \TeX\ knows it}
+/// show memory usage if `TeX` knows it
+pub(crate) const tracing_stats_code: halfword = 31;
 // @d tracing_paragraphs_code=32 {show line-break calculations}
+/// show line-break calculations
+pub(crate) const tracing_paragraphs_code: halfword = 32;
 // @d tracing_pages_code=33 {show page-break calculations}
+/// show page-break calculations
+pub(crate) const tracing_pages_code: halfword = 33;
 // @d tracing_output_code=34 {show boxes when they are shipped out}
+/// show boxes when they are shipped out
+pub(crate) const tracing_output_code: halfword = 34;
 // @d tracing_lost_chars_code=35 {show characters that aren't in the font}
 /// show characters that aren't in the font
 pub(crate) const tracing_lost_chars_code: halfword = 35;
@@ -66,30 +120,56 @@ pub(crate) const tracing_lost_chars_code: halfword = 35;
 /// show command codes at `big_switch`
 pub(crate) const tracing_commands_code: halfword = 36;
 // @d tracing_restores_code=37 {show equivalents when they are restored}
+/// show equivalents when they are restored
+pub(crate) const tracing_restores_code: halfword = 37;
 // @d uc_hyph_code=38 {hyphenate words beginning with a capital letter}
+/// hyphenate words beginning with a capital letter
+pub(crate) const uc_hyph_code: halfword = 38;
 // @d output_penalty_code=39 {penalty found at current page break}
+/// penalty found at current page break
+pub(crate) const output_penalty_code: halfword = 39;
 // @d max_dead_cycles_code=40 {bound on consecutive dead cycles of output}
+/// bound on consecutive dead cycles of output
+pub(crate) const max_dead_cycles_code: halfword = 40;
 // @d hang_after_code=41 {hanging indentation changes after this many lines}
 /// hanging indentation changes after this many lines
 pub(crate) const hang_after_code: halfword = 41;
 // @d floating_penalty_code=42 {penalty for insertions heldover after a split}
+/// penalty for insertions heldover after a split
+pub(crate) const floating_penalty_code: halfword = 42;
 // @d global_defs_code=43 {override \.{\\global} specifications}
 /// override `\global` specifications
 pub(crate) const global_defs_code: halfword = 43;
 // @d cur_fam_code=44 {current family}
+/// current family
+pub(crate) const cur_fam_code: halfword = 44;
 // @d escape_char_code=45 {escape character for token output}
 /// escape character for token output
 pub(crate) const escape_char_code: halfword = 45;
 // @d default_hyphen_char_code=46 {value of \.{\\hyphenchar} when a font is loaded}
+/// value of `\hyphenchar` when a font is loaded
+pub(crate) const default_hyphen_char_code: halfword = 46;
 // @d default_skew_char_code=47 {value of \.{\\skewchar} when a font is loaded}
+/// value of `\skewchar` when a font is loaded
+pub(crate) const default_skew_char_code: halfword = 47;
 // @d end_line_char_code=48 {character placed at the right end of the buffer}
 /// character placed at the right end of the buffer
 pub(crate) const end_line_char_code: halfword = 48;
 // @d new_line_char_code=49 {character that prints as |print_ln|}
+/// character that prints as `print_ln`
+pub(crate) const new_line_char_code: halfword = 49;
 // @d language_code=50 {current hyphenation table}
+/// current hyphenation table
+pub(crate) const language_code: halfword = 50;
 // @d left_hyphen_min_code=51 {minimum left hyphenation fragment size}
+/// minimum left hyphenation fragment size
+pub(crate) const left_hyphen_min_code: halfword = 51;
 // @d right_hyphen_min_code=52 {minimum right hyphenation fragment size}
+/// minimum right hyphenation fragment size
+pub(crate) const right_hyphen_min_code: halfword = 52;
 // @d holding_inserts_code=53 {do not remove insertion nodes from \.{\\box255}}
+/// do not remove insertion nodes from `\box255`
+pub(crate) const holding_inserts_code: halfword = 53;
 // @d error_context_lines_code=54 {maximum intermediate line pairs shown}
 /// maximum intermediate line pairs shown
 pub(crate) const error_context_lines_code: halfword = 54;
--- src/section_0238.rs
+++ src/section_0238.rs
@@ -5,65 +5,94 @@
 #[allow(unused_variables)]
 pub(crate) fn put_each_of_tex_s_primitivies_into_the_hash_table_0238(globals: &mut TeXGlobals) {
     // primitive("pretolerance",assign_int,int_base+pretolerance_code);@/
+    primitive(globals, strpool_str!("pretolerance"), assign_int, (int_base + pretolerance_code as word) as _);
     // @!@:pretolerance_}{\.{\\pretolerance} primitive@>
     // primitive("tolerance",assign_int,int_base+tolerance_code);@/
+    primitive(globals, strpool_str!("tolerance"), assign_int, (int_base + tolerance_code as word) as _);
     // @!@:tolerance_}{\.{\\tolerance} primitive@>
     // primitive("linepenalty",assign_int,int_base+line_penalty_code);@/
+    primitive(globals, strpool_str!("linepenalty"), assign_int, (int_base + line_penalty_code as word) as _);
     // @!@:line_penalty_}{\.{\\linepenalty} primitive@>
     // primitive("hyphenpenalty",assign_int,int_base+hyphen_penalty_code);@/
+    primitive(globals, strpool_str!("hyphenpenalty"), assign_int, (int_base + hyphen_penalty_code as word) as _);
     // @!@:hyphen_penalty_}{\.{\\hyphenpenalty} primitive@>
     // primitive("exhyphenpenalty",assign_int,int_base+ex_hyphen_penalty_code);@/
+    primitive(globals, strpool_str!("exhyphenpenalty"), assign_int, (int_base + ex_hyphen_penalty_code as word) as _);
     // @!@:ex_hyphen_penalty_}{\.{\\exhyphenpenalty} primitive@>
     // primitive("clubpenalty",assign_int,int_base+club_penalty_code);@/
+    primitive(globals, strpool_str!("clubpenalty"), assign_int, (int_base + club_penalty_code as word) as _);
     // @!@:club_penalty_}{\.{\\clubpenalty} primitive@>
     // primitive("widowpenalty",assign_int,int_base+widow_penalty_code);@/
+    primitive(globals, strpool_str!("widowpenalty"), assign_int, (int_base + widow_penalty_code as word) as _);
     // @!@:widow_penalty_}{\.{\\widowpenalty} primitive@>
     // primitive("displaywidowpenalty",
     //   assign_int,int_base+display_widow_penalty_code);@/
+    primitive(globals, strpool_str!("displaywidowpenalty"), assign_int, (int_base + display_widow_penalty_code as word) as _);
     // @!@:display_widow_penalty_}{\.{\\displaywidowpenalty} primitive@>
     // primitive("brokenpenalty",assign_int,int_base+broken_penalty_code);@/
+    primitive(globals, strpool_str!("brokenpenalty"), assign_int, (int_base + broken_penalty_code as word) as _);
     // @!@:broken_penalty_}{\.{\\brokenpenalty} primitive@>
     // primitive("binoppenalty",assign_int,int_base+bin_op_penalty_code);@/
+    primitive(globals, strpool_str!("binoppenalty"), assign_int, (int_base + bin_op_penalty_code as word) as _);
     // @!@:bin_op_penalty_}{\.{\\binoppenalty} primitive@>
     // primitive("relpenalty",assign_int,int_base+rel_penalty_code);@/
+    primitive(globals, strpool_str!("relpenalty"), assign_int, (int_base + rel_penalty_code as word) as _);
     // @!@:rel_penalty_}{\.{\\relpenalty} primitive@>
     // primitive("predisplaypenalty",assign_int,int_base+pre_display_penalty_code);@/
+    primitive(globals, strpool_str!("predisplaypenalty"), assign_int, (int_base + pre_display_penalty_code as word) as _);
     // @!@:pre_display_penalty_}{\.{\\predisplaypenalty} primitive@>
     // primitive("postdisplaypenalty",assign_int,int_base+post_display_penalty_code);@/
+    primitive(globals, strpool_str!("postdisplaypenalty"), assign_int, (int_base + post_display_penalty_code as word) as _);
     // @!@:post_display_penalty_}{\.{\\postdisplaypenalty} primitive@>
     // primitive("interlinepenalty",assign_int,int_base+inter_line_penalty_code);@/
+    primitive(globals, strpool_str!("interlinepenalty"), assign_int, (int_base + inter_line_penalty_code as word) as _);
     // @!@:inter_line_penalty_}{\.{\\interlinepenalty} primitive@>
     // primitive("doublehyphendemerits",
     //   assign_int,int_base+double_hyphen_demerits_code);@/
+    primitive(globals, strpool_str!("doublehyphendemerits"), assign_int, (int_base + double_hyphen_demerits_code as word) as _);
     // @!@:double_hyphen_demerits_}{\.{\\doublehyphendemerits} primitive@>
     // primitive("finalhyphendemerits",
     //   assign_int,int_base+final_hyphen_demerits_code);@/
+    primitive(globals, strpool_str!("finalhyphendemerits"), assign_int, (int_base + final_hyphen_demerits_code as word) as _);
     // @!@:final_hyphen_demerits_}{\.{\\finalhyphendemerits} primitive@>
     // primitive("adjdemerits",assign_int,int_base+adj_demerits_code);@/
+    primitive(globals, strpool_str!("adjdemerits"), assign_int, (int_base + adj_demerits_code as word) as _);
     // @!@:adj_demerits_}{\.{\\adjdemerits} primitive@>
     // primitive("mag",assign_int,int_base+mag_code);@/
+    primitive(globals, strpool_str!("mag"), assign_int, (int_base + mag_code as word) as _);
     // @!@:mag_}{\.{\\mag} primitive@>
     // primitive("delimiterfactor",assign_int,int_base+delimiter_factor_code);@/
+    primitive(globals, strpool_str!("delimiterfactor"), assign_int, (int_base + delimiter_factor_code as word) as _);
     // @!@:delimiter_factor_}{\.{\\delimiterfactor} primitive@>
     // primitive("looseness",assign_int,int_base+looseness_code);@/
+    primitive(globals, strpool_str!("looseness"), assign_int, (int_base + looseness_code as word) as _);
     // @!@:looseness_}{\.{\\looseness} primitive@>
     // primitive("time",assign_int,int_base+time_code);@/
+    primitive(globals, strpool_str!("time"), assign_int, (int_base + time_code as word) as _);
     // @!@:time_}{\.{\\time} primitive@>
     // primitive("day",assign_int,int_base+day_code);@/
+    primitive(globals, strpool_str!("day"), assign_int, (int_base + day_code as word) as _);
     // @!@:day_}{\.{\\day} primitive@>
     // primitive("month",assign_int,int_base+month_code);@/
+    primitive(globals, strpool_str!("month"), assign_int, (int_base + month_code as word) as _);
     // @!@:month_}{\.{\\month} primitive@>
     // primitive("year",assign_int,int_base+year_code);@/
+    primitive(globals, strpool_str!("year"), assign_int, (int_base + year_code as word) as _);
     // @!@:year_}{\.{\\year} primitive@>
     // primitive("showboxbreadth",assign_int,int_base+show_box_breadth_code);@/
+    primitive(globals, strpool_str!("showboxbreadth"), assign_int, (int_base + show_box_breadth_code as word) as _);
     // @!@:show_box_breadth_}{\.{\\showboxbreadth} primitive@>
     // primitive("showboxdepth",assign_int,int_base+show_box_depth_code);@/
+    primitive(globals, strpool_str!("showboxdepth"), assign_int, (int_base + show_box_depth_code as word) as _);
     // @!@:show_box_depth_}{\.{\\showboxdepth} primitive@>
     // primitive("hbadness",assign_int,int_base+hbadness_code);@/
+    primitive(globals, strpool_str!("hbadness"), assign_int, (int_base + hbadness_code as word) as _);
     // @!@:hbadness_}{\.{\\hbadness} primitive@>
     // primitive("vbadness",assign_int,int_base+vbadness_code);@/
+    primitive(globals, strpool_str!("vbadness"), assign_int, (int_base + vbadness_code as word) as _);
     // @!@:vbadness_}{\.{\\vbadness} primitive@>
     // primitive("pausing",assign_int,int_base+pausing_code);@/
+    primitive(globals, strpool_str!("pausing"), assign_int, (int_base + pausing_code as word) as _);
     // @!@:pausing_}{\.{\\pausing} primitive@>
     // primitive("tracingonline",assign_int,int_base+tracing_online_code);@/
     primitive(globals, strpool_str!("tracingonline"), assign_int, (int_base + tracing_online_code as word) as _);
@@ -72,12 +101,16 @@ pub(crate) fn put_each_of_tex_s_primitivies_into_the_hash_table_0238(globals: &m
     primitive(globals, strpool_str!("tracingmacros"), assign_int, (int_base + tracing_macros_code as word) as _);
     // @!@:tracing_macros_}{\.{\\tracingmacros} primitive@>
     // primitive("tracingstats",assign_int,int_base+tracing_stats_code);@/
+    primitive(globals, strpool_str!("tracingstats"), assign_int, (int_base + tracing_stats_code as word) as _);
     // @!@:tracing_stats_}{\.{\\tracingstats} primitive@>
     // primitive("tracingparagraphs",assign_int,int_base+tracing_paragraphs_code);@/
+    primitive(globals, strpool_str!("tracingparagraphs"), assign_int, (int_base + tracing_paragraphs_code as word) as _);
     // @!@:tracing_paragraphs_}{\.{\\tracingparagraphs} primitive@>
     // primitive("tracingpages",assign_int,int_base+tracing_pages_code);@/
+    primitive(globals, strpool_str!("tracingpages"), assign_int, (int_base + tracing_pages_code as word) as _);
     // @!@:tracing_pages_}{\.{\\tracingpages} primitive@>
     // primitive("tracingoutput",assign_int,int_base+tracing_output_code);@/
+    primitive(globals, strpool_str!("tracingoutput"), assign_int, (int_base + tracing_output_code as word) as _);
     // @!@:tracing_output_}{\.{\\tracingoutput} primitive@>
     // primitive("tracinglostchars",assign_int,int_base+tracing_lost_chars_code);@/
     primitive(globals, strpool_str!("tracinglostchars"), assign_int, (int_base + tracing_lost_chars_code as word) as _);
@@ -86,41 +119,58 @@ pub(crate) fn put_each_of_tex_s_primitivies_into_the_hash_table_0238(globals: &m
     primitive(globals, strpool_str!("tracingcommands"), assign_int, (int_base + tracing_commands_code as word) as _);
     // @!@:tracing_commands_}{\.{\\tracingcommands} primitive@>
     // primitive("tracingrestores",assign_int,int_base+tracing_restores_code);@/
+    primitive(globals, strpool_str!("tracingrestores"), assign_int, (int_base + tracing_restores_code as word) as _);
     // @!@:tracing_restores_}{\.{\\tracingrestores} primitive@>
     // primitive("uchyph",assign_int,int_base+uc_hyph_code);@/
+    primitive(globals, strpool_str!("uchyph"), assign_int, (int_base + uc_hyph_code as word) as _);
     // @!@:uc_hyph_}{\.{\\uchyph} primitive@>
     // primitive("outputpenalty",assign_int,int_base+output_penalty_code);@/
+    primitive(globals, strpool_str!("outputpenalty"), assign_int, (int_base + output_penalty_code as word) as _);
     // @!@:output_penalty_}{\.{\\outputpenalty} primitive@>
     // primitive("maxdeadcycles",assign_int,int_base+max_dead_cycles_code);@/
+    primitive(globals, strpool_str!("maxdeadcycles"), assign_int, (int_base + max_dead_cycles_code as word) as _);
     // @!@:max_dead_cycles_}{\.{\\maxdeadcycles} primitive@>
     // primitive("hangafter",assign_int,int_base+hang_after_code);@/
+    primitive(globals, strpool_str!("hangafter"), assign_int, (int_base + hang_after_code as word) as _);
     // @!@:hang_after_}{\.{\\hangafter} primitive@>
     // primitive("floatingpenalty",assign_int,int_base+floating_penalty_code);@/
+    primitive(globals, strpool_str!("floatingpenalty"), assign_int, (int_base + floating_penalty_code as word) as _);
     // @!@:floating_penalty_}{\.{\\floatingpenalty} primitive@>
     // primitive("globaldefs",assign_int,int_base+global_defs_code);@/
+    primitive(globals, strpool_str!("globaldefs"), assign_int, (int_base + global_defs_code as word) as _);
     // @!@:global_defs_}{\.{\\globaldefs} primitive@>
     // primitive("fam",assign_int,int_base+cur_fam_code);@/
+    primitive(globals, strpool_str!("fam"), assign_int, (int_base + cur_fam_code as word) as _);
     // @!@:fam_}{\.{\\fam} primitive@>
     // primitive("escapechar",assign_int,int_base+escape_char_code);@/
     primitive(globals, strpool_str!("escapechar"), assign_int, (int_base + escape_char_code as word) as _);
     // @!@:escape_char_}{\.{\\escapechar} primitive@>
     // primitive("defaulthyphenchar",assign_int,int_base+default_hyphen_char_code);@/
+    primitive(globals, strpool_str!("defaulthyphenchar"), assign_int, (int_base + default_hyphen_char_code as word) as _);
     // @!@:default_hyphen_char_}{\.{\\defaulthyphenchar} primitive@>
     // primitive("defaultskewchar",assign_int,int_base+default_skew_char_code);@/
+    primitive(globals, strpool_str!("defaultskewchar"), assign_int, (int_base + default_skew_char_code as word) as _);
     // @!@:default_skew_char_}{\.{\\defaultskewchar} primitive@>
     // primitive("endlinechar",assign_int,int_base+end_line_char_code);@/
+    primitive(globals, strpool_str!("endlinechar"), assign_int, (int_base + end_line_char_code as word) as _);
     // @!@:end_line_char_}{\.{\\endlinechar} primitive@>
     // primitive("newlinechar",assign_int,int_base+new_line_char_code);@/
+    primitive(globals, strpool_str!("newlinechar"), assign_int, (int_base + new_line_char_code as word) as _);
     // @!@:new_line_char_}{\.{\\newlinechar} primitive@>
     // primitive("language",assign_int,int_base+language_code);@/
+    primitive(globals, strpool_str!("language"), assign_int, (int_base + language_code as word) as _);
     // @!@:language_}{\.{\\language} primitive@>
     // primitive("lefthyphenmin",assign_int,int_base+left_hyphen_min_code);@/
+    primitive(globals, strpool_str!("lefthyphenmin"), assign_int, (int_base + left_hyphen_min_code as word) as _);
     // @!@:left_hyphen_min_}{\.{\\lefthyphenmin} primitive@>
     // primitive("righthyphenmin",assign_int,int_base+right_hyphen_min_code);@/
+    primitive(globals, strpool_str!("righthyphenmin"), assign_int, (int_base + right_hyphen_min_code as word) as _);
     // @!@:right_hyphen_min_}{\.{\\righthyphenmin} primitive@>
     // primitive("holdinginserts",assign_int,int_base+holding_inserts_code);@/
+    primitive(globals, strpool_str!("holdinginserts"), assign_int, (int_base + holding_inserts_code as word) as _);
     // @!@:holding_inserts_}{\.{\\holdinginserts} primitive@>
     // primitive("errorcontextlines",assign_int,int_base+error_context_lines_code);@/
+    primitive(globals, strpool_str!("errorcontextlines"), assign_int, (int_base + error_context_lines_code as word) as _);
     // @!@:error_context_lines_}{\.{\\errorcontextlines} primitive@>
 }
 
