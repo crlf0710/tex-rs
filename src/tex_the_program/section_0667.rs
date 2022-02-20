a3a430c61209b9b528d1b7c168e90e7abdba93d3
Charles Lew <crlf0710@gmail.com>
2022-02-20 11:34:59 +0800

    Fix bug in badness calculation reporting.

--- src/tex_the_program/section_0667.rs
+++ src/tex_the_program/section_0667.rs
@@ -3,7 +3,7 @@
 // @<Report a tight hbox and |goto common_ending|, if...@>=
 pub(crate) macro Report_a_tight_hbox_and_goto_common_ending__if_this_box_is_sufficiently_bad($globals:expr, $x:expr, $lbl_common_ending:lifetime) {{
     // begin last_badness:=badness(-x,total_shrink[normal]);
-    $globals.last_badness = badness($globals, $x, $globals.total_shrink[glue_ord::normal]) as _;
+    $globals.last_badness = badness($globals, -$x, $globals.total_shrink[glue_ord::normal]) as _;
     // if last_badness>hbadness then
     if $globals.last_badness > hbadness!($globals) {
         // begin print_ln; print_nl("Tight \hbox (badness "); print_int(last_badness);
